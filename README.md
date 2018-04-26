3aedd5b1a0866a8f5abee1eb6bd327e7e62bfb81
Felipe Rando <felipe.rando@rivendel.com.br>
2018-04-25 23:16:44 -0300

    pointers and stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+Things to remember about go
+![alt text](https://raw.githubusercontent.com/feliperando/go-workspace/master/pointers-in-a-nuthshell.png)
---
+++ src/structs/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact: contactInfo{
+			email:   "alex@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	alex.print()
+}
+
+func (p *person) changeName(name string) {
+	p.firstName = name
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p) //%+v prints all parameters with their values
+}
