54ae5ce5a5bd808a7fbb0709d421e984c8bd88b6
FelipeRando <feliperando2@gmail.com>
2018-05-28 20:10:13 -0300

    channels and go routines

--- src/channels/go-routine/main.go
+++ src/channels/go-routine/main.go
@@ -7,22 +7,32 @@ import (
 
 func main() {
 	links := []string{
+		"http://golang.org",
 		"http://google.com",
 		"http://facebook.com",
 		"http://amazon.com",
 		"http://stackoverflow.com",
 	}
+
+	cs := make(chan string) //create a channel
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, cs)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-cs)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, cs chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		cs <- "Might be down"
 		return
 	}
 
 	fmt.Println(link, "is up")
+	cs <- "Yep its up"
 }
