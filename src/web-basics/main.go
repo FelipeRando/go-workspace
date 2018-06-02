1335d21f817e62c378fa6cb9dc2072e3b9020a93
FelipeRando <feliperando2@gmail.com>
2018-06-01 23:26:37 -0300

    more on templates

--- src/web-basics/main.go
+++ src/web-basics/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"text/template"
+	"time"
 )
 
 type overwatchCharacter struct {
@@ -15,6 +17,13 @@ type overwatchCharacter struct {
 	Country      string
 }
 
+//FuncMap returns a variable of type FuncMap that maps names ("uc") into function("string.ToUpper") to be used in a template
+var fm = template.FuncMap{
+	"uc":         strings.ToUpper,
+	"formatTime": formatTime,
+	"double":     doubleMyNumber,
+}
+
 //tpl is the representation of a parsed Template
 //pointer of type Template of package template
 var tpl *template.Template
@@ -23,10 +32,11 @@ var tpl *template.Template
 //tpl variable
 func init() {
 	//must return a pointer of a type Template
-	tpl = template.Must(template.ParseGlob("templates/*")) //1-tpl holds all my parsed template file
+	tpl = template.Must(template.New("").Funcs(fm).ParseGlob("templates/*")) //1-tpl holds all my parsed template file
 }
 
 func main() {
+	start := time.Now()
 	defer fmt.Println("\nending now...")
 
 	slc := []string{"Alface", "Arroz", "Carne", "Batata"}
@@ -69,9 +79,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = tpl.ExecuteTemplate(os.Stdout, "footer.gohtml", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "function_on_template.gohtml", time.Now())
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	err = tpl.ExecuteTemplate(os.Stdout, "pipeline_on_template.gohtml", 3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "footer.gohtml", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Printf("\nelapsed: %v", elapsed)
+}
+
+func formatTime(t time.Time) string {
+	return t.Format("01-02-2006")
+}
+
+func doubleMyNumber(number int) int {
+	return number * 2
 }
