56ef3322983d23c651344d3413f71db000e05233
Felipe Rando <felipe.rando@rivendel.com.br>
2018-07-27 18:03:42 -0300

    added websocket + load test

--- src/websockets/main.go
+++ src/websockets/main.go
@@ -5,7 +5,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
 	indexFile, err := os.Open("html/index.html")
@@ -17,7 +25,31 @@ func main() {
 		fmt.Println(err)
 	}
 	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
-
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if string(msg) == "ping" {
+				fmt.Println("ping")
+				time.Sleep(2 * time.Second)
+				err = conn.WriteMessage(msgType, []byte("pong"))
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			} else {
+				conn.Close()
+				fmt.Println(string(msg))
+				return
+			}
+		}
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, string(index))
