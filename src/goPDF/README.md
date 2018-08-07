e6a2a397e64d058245d09cd0c38e3c7e942b3563
Felipe Rando <felipe.rando@rivendel.com.br>
2018-08-07 19:26:54 -0300

    started go pdf tests

---
+++ src/goPDF/README.md
@@ -0,0 +1,4 @@
+#goPDF-consulting
+Você é uma consultoria DevOps/Cloud e precisa fornecer um relatório de custos de forma prática, rapida e eficiente?
+Say no more!!!
+$PROJETO é uma implementação da biblioteca gofpdf criada com templates prontos para fornecer à seus clientes relatórios bonitos com todas as informações importantes sobre custos (e provavelmente algo mais no futuro)
--- src/goPDF/goPDF.go
+++ src/goPDF/goPDF.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
 
+	"github.com/jung-kurt/gofpdf"
+)
+
+func main() {
+	//Capa
+	pdf := newReport()
+	pdf.AddPage()
+	pdf = header(pdf)
+	err := pdf.OutputFileAndClose("hello.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func newReport() *gofpdf.Fpdf {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	return pdf
+}
+
+func header(pdf *gofpdf.Fpdf) *gofpdf.Fpdf {
+	pdf.SetFont("Arial", "B", 12)
+	pdf.SetFillColor(240, 240, 240)
+	pdf.Cell(40, 10, fmt.Sprintf("Cliente Relatório de Revisão de Custos %s", time.Now().Format("dd/mm/YYYY")))
+	return pdf
 }
