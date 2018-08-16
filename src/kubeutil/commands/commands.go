eac04a04a3181587e2a484dc387da63dad912fc0
Felipe Rando <felipe.rando@rivendel.com.br>
2018-08-15 22:55:39 -0300

    added cobra-cli

--- src/kubeutil/commands/commands.go
+++ src/kubeutil/commands/commands.go
@@ -1,11 +1,1 @@
 package commands
-
-import (
-	"flag"
-)
-
-func SaySomething() *string {
-	returnString := flag.String("say-something", "nada escrito", "type anything to say")
-	flag.Parse()
-	return returnString
-}
--- src/kubeutil/kubeutil.go
+++ src/kubeutil/kubeutil.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"kubeutil/commands"
+	"os"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	saySomething := *commands.SaySomething()
-	fmt.Println(saySomething)
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods("default").List(metav1.ListOptions{})
+	for _, pod := range pods.Items {
+		fmt.Println(pod.GetName())
+	}
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
